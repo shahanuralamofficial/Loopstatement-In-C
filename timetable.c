0f6d0252c28c0e7a40875aa51d7a862690058062
Md. Shahanur Alam <108014474+shahanuralamofficial@users.noreply.github.com>
2023-02-10 12:31:52 +0600

    Update timetable.c

--- timetable.c
+++ timetable.c
@@ -10,7 +10,8 @@ int main()
         scanf("%d",&num);
 
         for(i=1; i<=10; i++)
-        printf("%d X %d = %d",num,i,num*i);
+        printf("%d X %d = %d\n",num,i,num*i);
+        printf("\n");
     }
    return 0;
 }
